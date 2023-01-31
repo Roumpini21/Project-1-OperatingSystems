f284127578297127f7f244040c3e2e19c3163cd6
Jason Pavlopoulos <Jasonpavlop1@gmail.com>
2023-02-01 00:08:25 +0200

    Update logparser.sh

--- logparser.sh
+++ logparser.sh
@@ -11,7 +11,7 @@ awk '{count[$3]++} END {for (word in count) print count[word], word}' $1 | sort 
 
 # Define the count function
 count_browsers() {
-	line=$(awk '{ line = sprintf("%s %s %s %s %s %s %s %s %s %s %s %s %s %s %s", $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26) } END { print line }' $1)
+	line=$(awk '{ line = sprintf("%s %s %s %s %s %s %s %s %s %s %s %s %s", $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24) } END { print line }' $1)
 echo "Line: $line"
 mozilla=$(awk -v line="$line" '{ if (match(line, "Mozilla")) { mozilla++ } } END { print mozilla }')
 echo "Mozilla: $mozilla"
